b1d2101d5a459d0f0509231c45bf48e473268884
arshia Moghimi <arshiamoghimi98@gmail.com>
2021-03-03 15:35:58 +0330

    Refactored Coin.java

--- app/src/main/java/ir/sambal/coinify/Coin.java
+++ app/src/main/java/ir/sambal/coinify/Coin.java
@@ -1,21 +1,21 @@
 package ir.sambal.coinify;
 
 public class Coin {
-    public String name;
-    public String symbol;
-    public int price;
-    public int percent_change_1h;
-    public int percent_change_24h;
-    public int percent_change_7d;
-    public String imageURL;
+    private String name;
+    private String symbol;
+    private int price;
+    private int percent_change_1h;
+    private int percent_change_24h;
+    private int percent_change_7d;
+    private String imageURL;
 
     public Coin(String name, String symbol, int price, int percent_change_1h, int percent_change_24h, int percent_change_7d) {
-        this.name = name;
-        this.symbol = symbol;
-        this.price = price;
-        this.percent_change_1h = percent_change_1h;
-        this.percent_change_24h = percent_change_24h;
-        this.percent_change_7d = percent_change_7d;
+        this.setName(name);
+        this.setSymbol(symbol);
+        this.setPrice(price);
+        this.setPercent_change_1h(percent_change_1h);
+        this.setPercent_change_24h(percent_change_24h);
+        this.setPercent_change_7d(percent_change_7d);
     }
 
     public void setImageURL(String url) {
@@ -28,5 +28,49 @@ public class Coin {
 
     public String getSymbol() {
         return this.symbol;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPercent_change_1h() {
+        return percent_change_1h;
+    }
+
+    public void setPercent_change_1h(int percent_change_1h) {
+        this.percent_change_1h = percent_change_1h;
+    }
+
+    public int getPercent_change_24h() {
+        return percent_change_24h;
+    }
+
+    public void setPercent_change_24h(int percent_change_24h) {
+        this.percent_change_24h = percent_change_24h;
+    }
+
+    public int getPercent_change_7d() {
+        return percent_change_7d;
+    }
+
+    public void setPercent_change_7d(int percent_change_7d) {
+        this.percent_change_7d = percent_change_7d;
+    }
+
+    public String getImageURL() {
+        return imageURL;
     }
 }
